b0a0367c146a7d2449b44e723c53f260214dbc93
playatanu <playatanu@gmail.com>
2023-03-03 22:57:30 +0530

    port fixed

--- db.py
+++ db.py
@@ -3,7 +3,9 @@ import os
 
 db = mysql.connector.connect(
     host=os.getenv('MYSQL_HOST',default='127.0.0.1'),
-    port=os.getenv('MYSQL_PORT',default=3306),
+    # port=os.getenv('MYSQL_PORT',default=3306),
+    # only for port
+    port=6227,
     user=os.getenv('MYSQL_USER',default='root'),
     password=os.getenv('MYSQL_PASSWORD',default='root'),
     database=os.getenv('MYSQL_DB',default='test')
