91d92498449a92af9948142a0de005e1caa2a78b
playatanu <playatanu@gmail.com>
2023-03-03 22:42:40 +0530

    fixed

--- db.py
+++ db.py
@@ -1,10 +1,21 @@
-from main import mysql
+import mysql.connector
+import os
+
+db = mysql.connector.connect(
+    host=os.getenv('MYSQL_HOST',default='127.0.0.1'),
+    port=os.getenv('MYSQL_PORT',default=3306),
+    user=os.getenv('MYSQL_USER',default='root'),
+    password=os.getenv('MYSQL_PASSWORD',default='root'),
+    database=os.getenv('MYSQL_DB',default='test')
+)
 
 def getHouseListByCityName(city):
     sql = "select area,bed,bath,garage,price from properties where city= %s ORDER BY RAND() LIMIT 100"
     cityName = (city,)
-    cur=mysql.connection.cursor()
-    cur.execute(sql,cityName)
-    houseList = cur.fetchall()
-    cur.close()
+
+    cursor = db.cursor()
+    cursor.execute(sql,cityName)
+
+    houseList = cursor.fetchall()
+    cursor.close()
     return houseList
--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 from flask import Flask, jsonify,request
-from flask_mysqldb import MySQL
 import os
-
-import db
 import ml
 
 app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_PORT'] = 6227
-app.config['MYSQL_USER']= os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD']= os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB']= os.getenv('MYSQL_DB')
-app.config["MYSQL_CURSORCLASS"] = os.getenv('MYSQL_CURSORCLASS')
-
-mysql = MySQL(app)
-
 
 @app.route('/')
 def index():
@@ -27,4 +14,4 @@ def predect():
     return jsonify({"estimate": estimate})
   
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=os.getenv("PORT", default=8082))
+    app.run(debug=False,host='127.0.0.1', port=os.getenv("PORT", default=8082))
